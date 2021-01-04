510408bcad1059aef5d83d341069a33b7a434f97
MrtijnvanderBruggen <Martijnvanderbruggen@live.nl>
2021-01-04 16:05:46 +0100

    datatable with action buttons

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <Datatable />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Datatable from "@/components/Datatable.vue";
+
+export default {
+  name: "Home",
+  components: {
+    Datatable
+  }
+};
+</script>
